c28e16d406630a76900e28db169a18bb6103c549
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2026-08-16 01:21:43 +0530

    removed unused code and assets from the codebase

--- src/data/dropDown.js
+++
@@ -1,7 +0,0 @@
-const dropdownOptions = [
-  { id: 2, label: "Front-End Technology", value: "front-end technologies" },
-  { id: 3, label: "Libraries", value: "libraries" },
-  { id: 3, label: "Frameworks", value: "frameworks" },
-];
-
-export default dropdownOptions;
